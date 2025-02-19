43b432b39be7ae580f9b2ae03c837c237687e68c
Henrique Leunam Silva <henrique.leunam@hotmail.com>
2025-02-18 21:02:07 -0300

    Exemplo 1.1

---
+++ Cap 1 - O Cenário de Aprendizado de Máquina/Exemplo 1.1 - Trienamento e Execução.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+# Carrega os dados.
+oecd_bli_ = pd.read_csv('oecd_bli_2015.csv', thousands=',')
+gdp_per_capita = pd.read_csv('gdp_per_capita.csv', thousands=',', delimiter='\t', encoding='latin1', na_values='n/a')
+
+# Prepara os dados.
+country_stats = prepare_country_stats(oecd_bli_, gdp_per_capita)
+x = np.c_[country_stats["GDP per capita"]]
+y = np.c_[country_stats["Life satisfaction"]]
+
+# Visualiza os dados.
+country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
+plt.show()
+
+# Seleciona um modelo linear.
+model = sklearn.linear_model.LinearRegression()
+
+# Treina o modelo.
+model.fit(x, y)
+
+# Efetua uma predição para o Chipre.
+X_new = [[22587]]  # PIB per capita do Chipre.
+print(model.predict(X_new)) # Resultado esperado [[5.96242338]]
